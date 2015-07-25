93caa83a41eae23086631e641c4f253604dcdc06
stefan.buretea <sbpmail@yahoo.com>
2015-07-25 14:46:50 +0100

    Initial Commit

---
+++ WeatherTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherTest
+//
+//  Created by Stefan Buretea on 7/25/15.
+//  Copyright (c) 2015 Stefan Buretea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
